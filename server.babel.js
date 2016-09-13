dc2f0ae8d0bcaa94e803b30ccc3a0e14dc91a2dd
Alexandru Rosianu <me@aluxian.com>
2016-09-13 20:10:46 +0300

    Update deps

--- server.babel.js
+++ server.babel.js
@@ -1,25 +1,28 @@
 import path from 'path';
 import express from 'express';
 import graphQLHTTP from 'express-graphql';
+import { graphql } from 'graphql';
+import { graphqlSubscribe } from 'graphql-relay-subscription';
 import compression from 'compression';
 import cookieParser from 'cookie-parser';
+import Relay from 'react-relay';
 
 import routes from './src/routes';
-import { renderHTMLString, setNetworkLayer } from '@sketchpixy/rubix/lib/node/relay-router';
+import { renderHTMLString, setNetworkLayer } from './src/relay-router';
 import RubixAssetMiddleware from '@sketchpixy/rubix/lib/node/RubixAssetMiddleware';
-import schema from './data/schema.js';
+import { addNotifier } from './data/database';
+import schema from './data/schema';
 
 import GraphQLSettings from './graphql.json';
 
-let GraphQLEndpoint = GraphQLSettings.development.endpoint;
-
+let endpoint = GraphQLSettings.development.endpoint;
 if (process.env.NODE_ENV === 'production') {
-  GraphQLEndpoint = GraphQLSettings.production.endpoint;
+  endpoint = GraphQLSettings.production.endpoint;
 }
+endpoint = endpoint || 'http://localhost:8080/graphql';
+setNetworkLayer(new Relay.DefaultNetworkLayer(endpoint));
 
-setNetworkLayer(GraphQLEndpoint);
-
-const port = process.env.PORT || 8080;
+const PORT = process.env.PORT || 8080;
 
 let app = express();
 app.use(compression());
@@ -29,10 +32,7 @@ app.set('views', path.join(process.cwd(), 'views'));
 app.set('view engine', 'pug');
 
 app.use('/graphql', graphQLHTTP({schema, pretty: true}));
-
-app.get('/graphiql', (req, res, next) => {
-  res.render('graphiql');
-});
+app.get('/graphiql', (req, res) => res.render('graphiql'));
 
 function renderHTML(req, res) {
   renderHTMLString(routes, req, (error, redirectLocation, data) => {
@@ -53,10 +53,93 @@ function renderHTML(req, res) {
   });
 }
 
-app.get('*', RubixAssetMiddleware('ltr'), (req, res, next) => {
+app.get('*', RubixAssetMiddleware('ltr'), (req, res) => {
   renderHTML(req, res);
 });
 
-app.listen(port, () => {
-  console.log(`Node.js app is running at http://localhost:${port}/`);
+const graphQLServer = app.listen(PORT, () => {
+  console.log(`Node.js app is running at http://localhost:${PORT}/`);
+});
+
+const io = require('socket.io')(graphQLServer, {
+  serveClient: false,
+});
+
+io.on('connection', socket => {
+  const topics = Object.create(null);
+  const unsubscribeMap = Object.create(null);
+
+  const removeNotifier = addNotifier(({ topic, data }) => {
+    const topicListeners = topics[topic];
+    if (!topicListeners) return;
+
+    topicListeners.forEach(({ id, query, variables }) => {
+      graphql(
+        schema,
+        query,
+        data,
+        null,
+        variables
+      ).then((result) => {
+        socket.emit('subscription update', { id, ...result });
+      });
+    });
+  });
+
+  socket.on('subscribe', ({ id, query, variables }) => {
+    function unsubscribe(topic, subscription) {
+      const index = topics[topic].indexOf(subscription);
+      if (index === -1) return;
+
+      topics[topic].splice(index);
+
+      console.log(
+        'Removed subscription for topic %s. Total subscriptions for topic: %d',
+        topic,
+        topics[topic].length
+      );
+    }
+
+    function subscribe(topic) {
+      topics[topic] = topics[topic] || [];
+      const subscription = { id, query, variables };
+
+      topics[topic].push(subscription);
+
+      unsubscribeMap[id] = () => {
+        unsubscribe(topic, subscription);
+      };
+
+      console.log(
+        'New subscription for topic %s. Total subscriptions for topic: %d',
+        topic,
+        topics[topic].length
+      );
+    }
+
+    graphqlSubscribe({
+      schema,
+      query,
+      variables,
+      context: { subscribe },
+    }).then((result) => {
+      if (result.errors) {
+        console.error('Subscribe failed', result.errors);
+      }
+    });
+  });
+
+  socket.on('unsubscribe', (id) => {
+    const unsubscribe = unsubscribeMap[id];
+    if (!unsubscribe) return;
+
+    unsubscribe();
+    delete unsubscribeMap[id];
+    socket.emit('subscription closed', id);
+  });
+
+  socket.on('disconnect', () => {
+    console.log('Socket disconnect');
+    removeNotifier();
+  });
 });
